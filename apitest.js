a107945895d3916bc5d7215083f2228a839ea040
Heather Drake <heatherdrake@heathers-mbp.home>
2019-03-10 19:27:05 -0400

    api testing

---
+++ apitest.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	var accessToken = "0a88855990a24de5b775fd2c50d864e09ac38b71";
+	var longUrl = $('.long-url').attr('href');
+	var encodeLongUrl = encodeURIComponent(longUrl);
+	$.ajax({
+		method: "GET",
+		url: "https://api-ssl.bitly.com/v3/shorten?access_token="+accessToken+"&longUrl="+encodeLongUrl+"",
+		dataType: "json",
+		success: function(bitly){
+			var shortenedUrl = bitly.data.url;
+			console.log(bitly);
+			$('.short-url').attr('href',shortenedUrl);
+			var status = bitly.status_code;
+			if (status == 403) {
+				alert('Bitly Monthly Rate Limit Exceeded');
+			} else if (status == 500) {
+				alert("Missing Access Token. Sign in to Bitly and go to https://bitly.com/a/oauth_apps in order to get your access token");
+			}
+		},
+		error: function(){
+			alert('Bitly API was not able to shorten your URL');
+		}
+	});
+});
