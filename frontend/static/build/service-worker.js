b191a638db2a81602784f15becb5cca63cd96045
Heather Drake <heatherdrake@Heathers-MacBook-Pro.local>
2019-03-19 14:13:58 -0400

    added crud

--- frontend/static/build/service-worker.js
+++ frontend/static/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3548ffbc56e4f2f31ee6e67c473c821b.js"
+  "/precache-manifest.663cf10d86232f6741d5fc667b18d4e9.js"
 );
 
 workbox.clientsClaim();
--- frontend/static/src/container/App.js
+++ frontend/static/src/container/App.js
@@ -4,10 +4,54 @@ import ImageUpload from '../components/Form'
 
 class App extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts:[]
+        };
+
+        this.addPost=this.addPost.bind(this);
+    }
+
+    addPost(post) {
+        let formData = new FormData();
+        formData.append("image", post.image);
+        formData.append("blog", post.blog);
+
+
+        const conf = {
+            method: "POST",
+            body: formData,
+
+        };
+
+
+        fetch('/api/images/', conf).then((response) => {
+            return response.json();
+            // console.log("working", JSON);
+        }).then((json) => {
+
+            let posts = this.state.posts
+            posts.push(json)
+
+            this.setState({posts: posts})
+            console.log("working", json);
+
+            console.log('post worked', );
+        });
+    };
+
   render() {
+      let posts = this.state.posts.map(post=>{
+          return(
+              <div>I am a post</div>
+          )
+      })
     return (
         <div className="App">
-          <ImageUpload/>
+          <ImageUpload addPost={this.addPost}/>
+            {posts}
         </div>
     );
 
